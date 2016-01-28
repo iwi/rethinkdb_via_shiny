9f0962c386de9a84622a1360a2ed07c18994d614
iwi <iwi@tuta.io>
2016-01-28 18:07:07 +0000

    Change server and ui based new interactive stuff

--- form_app/ui.R
+++ form_app/ui.R
@@ -1,21 +1,20 @@
 library(shiny)
 
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  titlePanel("Hello Shiny!"),
-  # Sidebar with a slider input for the number of bins
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
-  # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
-    )
+  titlePanel("Input material information"),
+  fluidRow(
+      column(4, 
+        textInput(inputId,
+                  label = "name",
+                  value = "",
+                  width = NULL,
+                  placeholder = NULL)
+             wellPanel(
+      actionButton("submit", "submit")
+    )),
+  column(8, wellPanel(
+    verbatimTextOutput("inputText")
+    ))
   )
 ))
 
