25ec50086074d697331d5d91279818d099fae9c8
iwi <iwi@tuta.io>
2016-01-29 21:26:01 +0000

    Change server and ui from tutorial

--- form_app/server.R
+++ form_app/server.R
@@ -1,7 +1,31 @@
 library(shiny)
+library(rjson)
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+shinyServer(function(input, output){
+  output$nombre <- renderText({paste('Nombre:', input$nombre)})
+  output$nivel <- renderText({input$nivel})
+  output$material <- renderText({input$material})
   
-
+#  new_df_record <- df(
+#    'nombre' = output$nombre,
+#    'nivel' = output$nivel,
+#    'material' = output$material
+#  )
+#  new_json_record <- toJSON(new_df_record) 
+#  output$downloadData <- downloadHandler(
+#    filename = function() {
+#      paste('new.json')
+#    },
+#    
+#    # This function should write data to a file given to
+#    # it by the argument 'filename'.
+#    content = function(file) {
+#      # Write to a file specified by the 'file' argument
+#      print(file)
+#      writeLines(
+#        new_json_record,
+#        file
+#      )
+#    }
+#  )
 })
--- form_app/ui.R
+++ form_app/ui.R
@@ -1,20 +1,63 @@
 library(shiny)
 
-shinyUI(fluidPage(
-  titlePanel("Input material information"),
-  fluidRow(
-      column(4, 
-        textInput(inputId,
-                  label = "name",
-                  value = "",
-                  width = NULL,
-                  placeholder = NULL)
-             wellPanel(
-      actionButton("submit", "submit")
-    )),
-  column(8, wellPanel(
-    verbatimTextOutput("inputText")
-    ))
-  )
+shinyUI(navbarPage(
+  title = "Tatiana's teaching materials",
+  tabPanel(
+    "Summary",
+    h1('What a nice title'),
+    p('something and something'),
+    img(src = "nens.jpg",
+        height = 400,
+        width = 300)
+  ),
+  tabPanel('Input new material',
+    fluidPage(
+#      titlePanel(strong('Input nuevo material')),
+      sidebarLayout(
+        sidebarPanel(
+          textInput(
+            'nombre',
+            label = h3('Nombre del material'), 
+            value = '...'
+          ),
+          checkboxGroupInput(
+            "nivel",
+            label = h3('Nivel'), 
+            choices = list("Reception" = 'Reception', 
+                           "Year 1" = 'Year 1', 
+                           "Year 2" = 'Year 2',
+                           "Year 3" = 'Year 3',
+                           "Year 4" = 'Year 4',
+                           "Year 5" = 'Year 5',
+                           "Year 6" = 'Year 6',
+                           "Adults" = 'Adults'
+                           ),
+            selected = 1
+          )
+#          selectInput(
+#            "material",
+#            label = h3("Material"), 
+#            choices = list("Juego" = 'Juego',
+#                           "Canción" = 'Canción',
+#                           "Texto" = 'Texto',
+#                           "Otros" = 'Otros'), 
+#            selected = 1
+#          )
+        ),
+        mainPanel(
+          h3('Material a añadir'),
+          textOutput('nombre'),
+          br(),
+          textOutput('nivel'),
+          br(),
+          textOutput('material'),
+          br(),
+          submitButton("Añadir"),
+          br(),
+          downloadButton("Guardar")
+        )
+      )
+    )
+  ),
+  tabPanel("Search")
 ))
-
